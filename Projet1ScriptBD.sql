ec19d096f2fee970bac587b2938c68eecfda73ab
vincent constantin <von3007@live.ca>
2018-09-12 13:06:54 -0400

    ajout de constraints

--- Projet1ScriptBD.sql
+++ Projet1ScriptBD.sql
@@ -22,11 +22,11 @@ CREATE UNIQUE INDEX TypeSoin_NoTypeSoin_uindex ON TypeSoin (NoTypeSoin)
 
 CREATE TABLE Soin
 (
-    NoSoin INT PRIMARY KEY NOT NULL IDENTITY,
+    NoSoin INT PRIMARY KEY NOT NULL,
     Description VARCHAR(75),
     Duree TIME NOT NULL,
     NoTypeSoin INT NOT NULL FOREIGN KEY REFERENCES TypeSoin(NoTypeSoin),
-    Prix MONEY NOT NULL
+    Prix NUMERIC(5,2) NOT NULL
 )
 
 /*
@@ -34,7 +34,7 @@ CREATE TABLE Soin
  */
 CREATE TABLE Assistant
 (
-    NoAssistant INT PRIMARY KEY NOT NULL IDENTITY,
+    NoAssistant INT PRIMARY KEY NOT NULL,
     Prenom VARCHAR(25) NOT NULL,
     Nom VARCHAR(25) NOT NULL,
     Specialites VARCHAR(20),
@@ -54,45 +54,34 @@ CREATE TABLE AssistantSoin
 )
 
 /*
-| Creation de la table PlanifSoin
- */
-CREATE TABLE PlanifSoin
-(
-    NoPersonne INT NOT NULL,
-    NoAssistant INT NOT NULL,
-    DateHeure DATETIME NOT NULL,
-    NoSoin INT NOT NULL,
-	CONSTRAINT PK_PlanifSoin PRIMARY KEY (NoPersonne,NoAssistant,DateHeure),
-    CONSTRAINT PlanifSoin_Soin_NoSoin_fk FOREIGN KEY (NoSoin) REFERENCES Soin (NoSoin),
-    CONSTRAINT PlanifSoin_Assistant_NoAssistant_fk FOREIGN KEY (NoAssistant) REFERENCES Assistant (NoAssistant)
-)
-
-/*
 | Creation de la table Client
  */
 CREATE TABLE Client
 (
-    NoCliet INT PRIMARY KEY NOT NULL IDENTITY,
+    NoClient INT PRIMARY KEY NOT NULL,
     Nom VARCHAR(25) NOT NULL,
     Prenom VARCHAR(25) NOT NULL,
     Ville VARCHAR(25) NOT NULL,
     Pays VARCHAR(25) NOT NULL,
     Adresse VARCHAR(25),
     CodePostal VARCHAR(25),
-    Description VARCHAR(25) NOT NULL
+    Description VARCHAR(25) NOT NULL,
+	CONSTRAINT CK_NoClient CHECK(NoClient%10 = 0) 
 )
-CREATE UNIQUE INDEX Client_NoCliet_uindex ON Client (NoCliet)
+CREATE UNIQUE INDEX Client_NoCliet_uindex ON Client (NoClient)
 
 /*
 | Creation de la table Invite
  */
 CREATE TABLE Invite
 (
-    NoInvite INT PRIMARY KEY NOT NULL IDENTITY,
+    NoInvite INT PRIMARY KEY NOT NULL,
     Nom VARCHAR(25) NOT NULL,
     Prenom VARCHAR(25) NOT NULL,
     NoClient INT,
-    CONSTRAINT Invite_Client_NoCliet_fk FOREIGN KEY (NoClient) REFERENCES Client (NoCliet)
+    CONSTRAINT Invite_Client_NoCliet_fk FOREIGN KEY (NoClient) REFERENCES Client (NoClient),
+	CONSTRAINT CK_NoInvite CHECK(NoInvite % 10 != 0),
+	CONSTRAINT CK_NoInvite2 CHECK(Noclient - NoInvite >= 9)
 )
 CREATE UNIQUE INDEX Invite_NoInvite_uindex ON Invite (NoInvite)
 
@@ -101,11 +90,11 @@ CREATE UNIQUE INDEX Invite_NoInvite_uindex ON Invite (NoInvite)
 */
 CREATE TABLE TypeChambre
 (
-    NoTypeChambre INT PRIMARY KEY NOT NULL IDENTITY,
+    NoTypeChambre INT PRIMARY KEY NOT NULL,
     Description VARCHAR(100),
-    PrixHaut MONEY NOT NULL,
-    PrixBas MONEY NOT NULL,
-    PrixMoyen MONEY NOT NULL
+    PrixHaut NUMERIC(5,2) NOT NULL,
+    PrixBas NUMERIC(5,2) NOT NULL,
+    PrixMoyen NUMERIC(5,2) NOT NULL
 )
 CREATE UNIQUE INDEX TypeChambre_NoTypeChambre_uindex ON TypeChambre (NoTypeChambre)
 
@@ -130,10 +119,26 @@ CREATE TABLE ReservationChambre
 (
     NoClient INT NOT NULL,
     NoChambre INT NOT NULL,
-    DateArrivee DATE NOT NULL,
-    DateDepart DATE NOT NULL,
+    DateArrivee DATETIME NOT NULL,
+    DateDepart DATETIME NOT NULL,
     NbPersonnes INT NOT NULL,
 	CONSTRAINT PK_ReservationChambre PRIMARY KEY (NoClient,NoChambre,DateArrivee),
-    CONSTRAINT ReservationChambre_Client_NoCliet_fk FOREIGN KEY (NoClient) REFERENCES Client (NoCliet),
+    CONSTRAINT ReservationChambre_Client_NoCliet_fk FOREIGN KEY (NoClient) REFERENCES Client (NoClient),
     CONSTRAINT ReservationChambre_Chambre_NoChambre_fk FOREIGN KEY (NoChambre) REFERENCES Chambre (NoChambre)
 )
+/*
+| Creation de la table PlanifSoin
+ */
+CREATE TABLE PlanifSoin
+(
+    NoPersonne INT NOT NULL,
+    NoAssistant INT NOT NULL,
+    DateHeure DATETIME NOT NULL,
+    NoSoin INT NOT NULL,
+	CONSTRAINT PK_PlanifSoin PRIMARY KEY (NoPersonne,NoAssistant,DateHeure),
+    CONSTRAINT PlanifSoin_Soin_NoSoin_fk FOREIGN KEY (NoSoin) REFERENCES Soin (NoSoin),
+    CONSTRAINT PlanifSoin_Assistant_NoAssistant_fk FOREIGN KEY (NoAssistant) REFERENCES Assistant (NoAssistant),
+	CONSTRAINT PlanifSoin_Invite_NoPersonne_fk FOREIGN KEY (NoPersonne) REFERENCES Invite (NoInvite),
+    CONSTRAINT PlanifSoin_Client_NoPersonne_fk FOREIGN KEY (NoPersonne) REFERENCES Client (NoClient)
+)
+
