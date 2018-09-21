120b68ff5dd8c245d76eef8a04c9a93abd22435f
vincent constantin <von3007@live.ca>
2018-09-21 16:43:43 -0400

    modification interface gestionAssistant

---
+++ Projet1-5B6/Forms Commun/Forms Gestion Assistants/FrmAddAssistant.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Projet1_5B6.Forms_Admin.Forms_Gestion_Assistants
+{
+    partial class FrmAddAssistant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPrenom = new System.Windows.Forms.TextBox();
+            this.tbRemarques = new System.Windows.Forms.TextBox();
+            this.tbSpecialite = new System.Windows.Forms.TextBox();
+            this.tbNom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblErrorProvider = new System.Windows.Forms.Label();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnConfirmer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPrenom
+            // 
+            this.tbPrenom.Location = new System.Drawing.Point(102, 12);
+            this.tbPrenom.Name = "tbPrenom";
+            this.tbPrenom.Size = new System.Drawing.Size(124, 20);
+            this.tbPrenom.TabIndex = 0;
+            // 
+            // tbRemarques
+            // 
+            this.tbRemarques.Location = new System.Drawing.Point(102, 90);
+            this.tbRemarques.Name = "tbRemarques";
+            this.tbRemarques.Size = new System.Drawing.Size(124, 20);
+            this.tbRemarques.TabIndex = 2;
+            // 
+            // tbSpecialite
+            // 
+            this.tbSpecialite.Location = new System.Drawing.Point(102, 64);
+            this.tbSpecialite.Name = "tbSpecialite";
+            this.tbSpecialite.Size = new System.Drawing.Size(124, 20);
+            this.tbSpecialite.TabIndex = 3;
+            // 
+            // tbNom
+            // 
+            this.tbNom.Location = new System.Drawing.Point(102, 38);
+            this.tbNom.Name = "tbNom";
+            this.tbNom.Size = new System.Drawing.Size(124, 20);
+            this.tbNom.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Prénom * :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nom * :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Spécialité * :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Remarques :";
+            // 
+            // lblErrorProvider
+            // 
+            this.lblErrorProvider.AutoSize = true;
+            this.lblErrorProvider.ForeColor = System.Drawing.Color.Black;
+            this.lblErrorProvider.Location = new System.Drawing.Point(22, 124);
+            this.lblErrorProvider.Name = "lblErrorProvider";
+            this.lblErrorProvider.Size = new System.Drawing.Size(192, 13);
+            this.lblErrorProvider.TabIndex = 9;
+            this.lblErrorProvider.Text = "Les champs avec un * sont obligatoires";
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.Color.Red;
+            this.btnAnnuler.Location = new System.Drawing.Point(13, 141);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(102, 41);
+            this.btnAnnuler.TabIndex = 10;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
+            // 
+            // btnConfirmer
+            // 
+            this.btnConfirmer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConfirmer.Location = new System.Drawing.Point(124, 141);
+            this.btnConfirmer.Name = "btnConfirmer";
+            this.btnConfirmer.Size = new System.Drawing.Size(102, 41);
+            this.btnConfirmer.TabIndex = 11;
+            this.btnConfirmer.Text = "Confirmer";
+            this.btnConfirmer.UseVisualStyleBackColor = false;
+            this.btnConfirmer.Click += new System.EventHandler(this.btnConfirmer_Click);
+            // 
+            // FrmAddAssistant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 196);
+            this.Controls.Add(this.btnConfirmer);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.lblErrorProvider);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNom);
+            this.Controls.Add(this.tbSpecialite);
+            this.Controls.Add(this.tbRemarques);
+            this.Controls.Add(this.tbPrenom);
+            this.Name = "FrmAddAssistant";
+            this.Text = "FrmAddAssistant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPrenom;
+        private System.Windows.Forms.TextBox tbRemarques;
+        private System.Windows.Forms.TextBox tbSpecialite;
+        private System.Windows.Forms.TextBox tbNom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblErrorProvider;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnConfirmer;
+    }
+}
---
+++ Projet1-5B6/Forms Commun/Forms Gestion Assistants/FrmGestionAssistants.cs
@@ -0,0 +1,126 @@
+﻿using Projet1_5B6.Forms_Admin.Forms_Gestion_Assistants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet1_5B6.Forms_Admin
+{
+    public partial class FrmGestionAssistants : Form
+    {
+        public FrmGestionAssistants()
+        {
+            InitializeComponent();
+        }
+
+        private void assistantBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.assistantBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bD5B6TP1_ConstantinBrassardLaheyDataSet);
+
+        }
+
+        private void FrmGestionAssistants_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistanSoinDescription' table. You can move, or remove it, as needed.
+            this.assistanSoinDescriptionTableAdapter.Fill(this.bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistanSoinDescription);
+            // TODO: This line of code loads data into the 'bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistantSoin' table. You can move, or remove it, as needed.
+            this.assistantSoinTableAdapter.Fill(this.bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistantSoin);
+            // TODO: This line of code loads data into the 'bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistantSoin' table. You can move, or remove it, as needed.
+            this.assistantSoinTableAdapter.Fill(this.bD5B6TP1_ConstantinBrassardLaheyDataSet.AssistantSoin);
+            // TODO: This line of code loads data into the 'bD5B6TP1_ConstantinBrassardLaheyDataSet.Assistant' table. You can move, or remove it, as needed.
+            this.assistantTableAdapter.Fill(this.bD5B6TP1_ConstantinBrassardLaheyDataSet.Assistant);
+
+            foreach (DataGridViewRow row in assistantDataGridView.Rows)
+            {
+                row.ReadOnly = true;
+            }
+
+        }
+
+        private void btnAjouterAssistant_Click(object sender, EventArgs e)
+        {
+            new FrmAddAssistant().ShowDialog();
+            this.assistantTableAdapter.Fill(this.bD5B6TP1_ConstantinBrassardLaheyDataSet.Assistant);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSupprimerAssistant_Click(object sender, EventArgs e)
+        {
+            if (assistantDataGridView.SelectedRows.Count < 1)
+            {
+                MessageBox.Show(string.Format("Vous devez sélectionner au minimum 1 assistant"));
+                return;
+            }               
+
+            //confirmation du delete
+            DialogResult myResult;
+            myResult = MessageBox.Show("Voulez-vous vraiment supprimer cet assistant?", "Supprimer confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (myResult == DialogResult.OK)
+            {
+                // confirm delete
+                foreach (DataGridViewRow row in assistantDataGridView.SelectedRows)
+                {
+                    //get row index and noAssistant
+                    int intRowIndex = row.Index;
+                    string strNoAssistant = Convert.ToString(row.Cells[0].Value);
+
+                    //connection a la BD
+                    String maChaineConnexion = "Data Source=sqlinfo.cgodin.qc.ca;Initial Catalog=BD5B6TP1_ConstantinBrassardLahey;User ID=5B6Constantin;Password=Password1";
+
+                    //supprimer de la BD et datagridview
+                    try
+                    {
+                        using (SqlConnection con = new SqlConnection(maChaineConnexion))
+                        {
+                            con.Open();
+                            using (SqlCommand command = new SqlCommand("DELETE FROM ASSISTANT WHERE NoAssistant = '" + strNoAssistant + "'", con))
+                            {
+                                command.ExecuteNonQuery();
+                                assistantDataGridView.Rows.RemoveAt(row.Index);
+                            }
+                            con.Close();
+                        }
+                    }
+                    catch (SystemException ex)
+                    {
+                        MessageBox.Show(string.Format("Une erreur est survenue: ", ex.Message));
+                    }
+                }
+            }           
+        }
+
+        private void btnModifierAssistant_Click(object sender, EventArgs e)
+        {
+            if(assistantDataGridView.SelectedRows.Count > 1)
+            {
+                MessageBox.Show(string.Format("Vous pouvez uniquement modifier un assistant à la fois"));
+                return;
+            }
+            if (assistantDataGridView.SelectedRows.Count < 1)
+            {
+                MessageBox.Show(string.Format("Vous devez sélectionner un assistant pour le modifier"));
+                return;
+            }
+            DataGridViewRow row = assistantDataGridView.SelectedRows[0];
+            row.ReadOnly = false;
+            row.DefaultCellStyle.BackColor = Color.LightBlue;
+        }
+    }
+}
