8f427a44e86c860242be6519cbfc6d326ad9ff89
Antoine Brassard <antoine.brassard@gmail.com>
2018-10-04 12:24:59 -0400

    Ajout ADOUtils + SupprimerSelection

---
+++ Projet1-5B6/ADOUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet1_5B6
+{
+    public static class ADOUtils
+    {
+        public static void SupprimerSelection(BindingSource bindingSource, Predicate<DataRowView> estSupprimable)
+        {
+            DataRowView selection = (DataRowView)bindingSource.Current;
+
+            if (!estSupprimable(selection)) return;
+
+            selection.Delete();
+        }
+    }
+}
